d79e5f7d0bcfde0914280110bd72b939e8336496
ericlchen <ericlchen@utexas.edu>
2021-08-23 14:18:58 -0500

    Added basic template

---
+++ popup.js
@@ -0,0 +1,40 @@
+const changeColor = document.getElementById("colorChange");
+const buttonOptions = document.getElementById("buttonDiv");
+const selectedClassName = "current";
+const buttonColors = ["000000"];
+
+chrome.storage.sync.get("color", ({color}) => {
+    changeColor.style.backgroundColor = color;
+});
+
+function handleButtonClick(e) {
+    const current = e.target.parentElement.querySelector(`.${selectedClassName}`)
+    if (current && current !== e.target) {
+        current.classList.remove(selectedClassName);
+    }
+
+    const color = e.target.dataset.color;
+    e.target.classList.add(selectedClassName);
+    chrome.storage.sync.set({ color });
+    changeColor.style.backgroundColor = color;
+};
+
+function constructOptions(buttonColors) {
+    chrome.storage.sync.get("color", (data) => {
+        const currentColor = data.color;
+        for (let buttonColor of buttonColors) {
+            const button = document.createElement("button");
+            button.dataset.color = buttonColor;
+            button.style.backgroundColor = buttonColor;
+
+            if (buttonColor === currentColor) {
+                button.classList.add(selectedClassName);
+            }
+
+            button.addEventListener("click", handleButtonClick);
+            buttonOptions.appendChild(button);
+        }
+    })
+};
+
+constructOptions(buttonColors);
